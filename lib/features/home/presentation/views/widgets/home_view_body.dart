ebe1149f4fb270eb06f841314ba099560c55aaf2
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-09-30 22:31:00 +0200

    completed home view widgets (design part)

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,35 +1,69 @@
 import 'package:Basera/core/widgets/custom_search_field.dart';
+import 'package:Basera/features/home/presentation/views/widgets/categories_section.dart';
 import 'package:Basera/features/home/presentation/views/widgets/custom_carouse_slider.dart';
 import 'package:Basera/features/home/presentation/views/widgets/home_app_bar.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:Basera/features/home/presentation/views/widgets/home_view_body.dart';
+import 'package:Basera/features/home/presentation/views/widgets/most_ordered_list_view.dart';
+import 'package:Basera/features/home/presentation/views/widgets/section_title.dart';
+import 'package:Basera/features/home/presentation/views/widgets/work_shop_section.dart';
 import 'package:flutter/material.dart';
-
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
+    return Scaffold(
       appBar: const HomeAppBar(),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 24),
-            child: CustomSearchField(hintText: 'ابحث..',icon: Container(
-             decoration:  const BoxDecoration(
-               color: Colors.black,
-               shape: BoxShape.circle,
-             ),
-                child: const Icon(Icons.search ,color: Colors.white,size: 15,)),),
-          ),
-          const SizedBox(height: 24,),
-          const CustomCarouseSlider()
-
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 24,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: CustomSearchField(
+                hintText: 'ابحث..',
+                icon: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.onSurface,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(
+                      Icons.search,
+                      color: Theme.of(context).colorScheme.surface,
+                      size: 15,
+                    )),
+              ),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            const CustomCarouseSlider(),
+            const SizedBox(
+              height: 32,
+            ),
+            const CategoriesSection(),
+            const SizedBox(
+              height: 32,
+            ),
+            const WorkShopsSection(),
+            const SizedBox(
+              height: 32,
+            ),
+            const SectionTitle(title: 'الاكثر طلبا'),
+            const SizedBox(
+              height: 8,
+            ),
+            const MostOrderedListView()
+          ],
+        ),
       ),
     );
   }
 }
+
+
 
 
