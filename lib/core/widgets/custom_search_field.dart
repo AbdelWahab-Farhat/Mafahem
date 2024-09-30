1e4ae97ff561f5b0b29416bd333550ed2176d4ac
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-09-30 22:33:01 +0200

    changed height to 50, added tag for hero widget and more customizations

--- lib/core/widgets/custom_search_field.dart
+++ lib/core/widgets/custom_search_field.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 class CustomSearchField extends StatelessWidget {
   final String? hintText;
   final Widget? icon;
+  final String tag = "search";
   const CustomSearchField({
     super.key,
     this.hintText,
@@ -14,22 +15,29 @@ class CustomSearchField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 50,
-      child: TextFormField(
-        enabled: false,
-        keyboardType:
-        icon == null ? TextInputType.text : TextInputType.number,
-        decoration: InputDecoration(
-          hintText: hintText,
-          hintStyle: Styles.style14(context).copyWith(color: const Color(0xff4D616B)),
-          fillColor: Colors.white,
-          filled: true,
-          enabledBorder: Styles.setOutlinedBorder(context, radius: 16),
-          disabledBorder: Styles.setOutlinedBorder(context , radius: 16),
-          focusedBorder: Styles.setOutlinedBorder(context ,width: 2),
-          suffixIcon: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: icon,
+      height: 40,
+      child: Hero(
+        tag: tag,
+        child: TextFormField(
+          enabled: false,
+          keyboardType:
+          icon == null ? TextInputType.text : TextInputType.number,
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            hintText: hintText,
+            hintStyle: Styles.style14(context).copyWith(color: const Color(0xff4D616B)),
+            fillColor: Colors.white,
+            filled: true,
+            enabledBorder: Styles.setOutlinedBorder(context, radius: 16),
+            disabledBorder: Styles.setOutlinedBorder(context, radius: 16),
+            focusedBorder: Styles.setOutlinedBorder(context, width: 2),
+            suffixIcon: Container(
+              margin: const EdgeInsets.only(left: 10),
+                child: icon),
+            suffixIconConstraints: const BoxConstraints(
+              minHeight: 40,
+              minWidth: 40,
+            ),
           ),
         ),
       ),
