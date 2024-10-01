6571f280c73757a6d17a8ebd0966339f669602d8
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-01 20:53:23 +0200

    added sizedBox to text widget to make it responsive

--- lib/features/cart/presentation/views/widgets/cart_item_widget_content.dart
+++ lib/features/cart/presentation/views/widgets/cart_item_widget_content.dart
@@ -1,3 +1,4 @@
+import 'package:Basera/core/utility/size_config.dart';
 import 'package:Basera/core/utility/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -11,18 +12,24 @@ class CartItemWidgetContent extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          "الكيمياء حيوية",
-          style: Styles.style16(context)
-              .copyWith(fontWeight: FontWeight.w600),
+        SizedBox(
+          width: SizeConfig.screenWidth * 0.5,
+          child: Text(
+              "الكيمياء حيوية",
+              style: Styles.style16(context)
+                  .copyWith(fontWeight: FontWeight.w600),
+            ),
         ),
         const SizedBox(
           height: 4,
         ),
-        Text(
-          'المحاضر:عزيز احمد',
-          style: Styles.style14(context).copyWith(
-              color: Theme.of(context).colorScheme.secondary),
+        SizedBox(
+          width: SizeConfig.screenWidth * 0.5,
+          child: Text(
+            'المحاضر:عزيز احمد',
+            style: Styles.style14(context).copyWith(
+                color: Theme.of(context).colorScheme.secondary),
+          ),
         )
       ],
     );
