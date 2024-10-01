7a460ad2f5c21697e0af2d926cce6004ca68c7d8
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-01 16:24:40 +0200

    added navigate to cart

--- lib/features/home/presentation/views/widgets/home_app_bar.dart
+++ lib/features/home/presentation/views/widgets/home_app_bar.dart
@@ -1,5 +1,7 @@
+import 'package:Basera/core/utility/helper.dart';
 import 'package:Basera/core/utility/size_config.dart';
 import 'package:Basera/core/utility/styles.dart';
+import 'package:Basera/features/cart/presentation/views/cart_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -47,7 +49,7 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
         IconButton(
             onPressed: () {}, icon: SvgPicture.asset('lib/assets/icons/notification.svg',height: 25,width: 25,color: Theme.of(context).colorScheme.onSurface,)),
         IconButton(
-            onPressed: () {}, icon: SvgPicture.asset('lib/assets/icons/cart.svg',height: 25,width: 25,color: Theme.of(context).colorScheme.onSurface,)),
+            onPressed: () => push(context, const CartView()), icon: SvgPicture.asset('lib/assets/icons/cart.svg',height: 25,width: 25,color: Theme.of(context).colorScheme.onSurface,)),
       ],
       shape:  LinearBorder(side:  BorderSide(width: 0.2,color: Theme.of(context).colorScheme.onSurface),bottom: LinearBorderEdge()),
     );
