251c209f9f6787938caa845a588dcc48a007d34d
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-01 16:19:19 +0200

    added bloc provider (scroll cubit)

---
+++ lib/features/cart/presentation/views/cart_view.dart
@@ -0,0 +1,16 @@
+import 'package:Basera/features/cart/presentation/manager/scroll_cubit/scroll_cubit.dart';
+import 'package:Basera/features/cart/presentation/views/widgets/cart_view_body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CartView extends StatelessWidget {
+  const CartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ScrollCubit(),
+      child: const CartViewBody(),
+    );
+  }
+}
