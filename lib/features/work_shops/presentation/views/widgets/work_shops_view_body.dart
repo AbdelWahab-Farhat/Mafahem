735cc33480ecc7b5cf77ffd9564b62f5f65ccadc
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-03 12:52:07 +0200

    completed design

---
+++ lib/features/work_shops/presentation/views/widgets/work_shops_view_body.dart
@@ -0,0 +1,42 @@
+import 'package:Basera/core/utility/size_config.dart';
+import 'package:Basera/core/utility/styles.dart';
+import 'package:Basera/core/widgets/custom_app_bar.dart';
+import 'package:Basera/features/work_shops/presentation/views/widgets/monthly_work_shop_list_view.dart';
+import 'package:Basera/features/work_shops/presentation/views/widgets/work_shop_Item.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WorkShopsViewBody extends StatelessWidget {
+  const WorkShopsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'ورش العـمل'),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        children: [
+          const WorkShopItem(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'ورش عمل شهرية',
+                style: Styles.style18(context)
+                    .copyWith(fontFamily: GoogleFonts.changa().fontFamily),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const MonthlyWorkShopListView(),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
---
+++ lib/features/work_shops/presentation/views/work_shops_view.dart
@@ -0,0 +1,11 @@
+import 'package:Basera/features/work_shops/presentation/views/widgets/work_shops_view_body.dart';
+import 'package:flutter/material.dart';
+
+class WorkShopsView extends StatelessWidget {
+  const WorkShopsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const WorkShopsViewBody();
+  }
+}
