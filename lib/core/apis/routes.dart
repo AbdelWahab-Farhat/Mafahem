0edcc43555891051555bc6ceb97c5b4a4f8a8c66
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-04 19:43:09 +0200

    routes Strings

---
+++ lib/core/apis/routes.dart
@@ -0,0 +1,8 @@
+
+
+abstract class Routes {
+  static const String BASE_URL = 'http://192.168.1.98:8000/api';
+  static const String LOGIN_URL = '$BASE_URL/login';
+  static const String REGISTER_URL = '$BASE_URL/register';
+  static const String USER_BY_TOKEN = '$BASE_URL/me';
+}
