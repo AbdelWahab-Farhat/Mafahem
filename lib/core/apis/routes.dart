bd93b0ca47c386432e90ec47b615278c33b12abb
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-05 00:11:37 +0200

    changed the baseURL

--- lib/core/apis/routes.dart
+++ lib/core/apis/routes.dart
@@ -1,7 +1,7 @@
 
 
 abstract class Routes {
-  static const String BASE_URL = 'http://192.168.1.98:8000/api';
+  static const String BASE_URL = 'http://192.168.9.140:3000/api';
   static const String LOGIN_URL = '$BASE_URL/login';
   static const String REGISTER_URL = '$BASE_URL/register';
   static const String USER_BY_TOKEN = '$BASE_URL/me';
