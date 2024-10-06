3d8972d118781a489f93fb4217852bcfc6669890
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-06 21:49:23 +0200

    logic and data for forget password view

---
+++ lib/features/Auth/presentation/manager/forget_password_cubit/forget_password_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:Basera/features/Auth/data/rest_password_service.dart';
+import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:meta/meta.dart';
+
+part 'forget_password_state.dart';
+
+class ForgetPasswordCubit extends Cubit<ForgetPasswordState> {
+   String? email;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  ForgetPasswordCubit() : super(ForgetPasswordInitial());
+
+
+  void sendRestPasswordOnEmail() async{
+    emit(ForgetPasswordLoading());
+    var result = await RestPasswordService(dio: GetIt.instance.get<Dio>()).restPassword(email!);
+    result.fold(
+      (l) => emit(ForgetPasswordFailure(errMessage: l.message)),
+      (r) => emit(ForgetPasswordSuccess())
+    );
+  }
+}
