546121a64ff188753878aa5e8252d922c3cfb5f5
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-06 21:50:17 +0200

    function service for forget password view

---
+++ lib/features/Auth/data/rest_password_service.dart
@@ -0,0 +1,30 @@
+
+
+import 'dart:io';
+
+import 'package:Basera/core/apis/routes.dart';
+import 'package:Basera/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class RestPasswordService {
+  final Dio dio;
+  RestPasswordService({required this.dio});
+  
+  Future<Either<Failure, String>> restPassword(String email) async{
+    try {
+      var res = await dio.post(Routes.REST_PASSWORD_URL,data: {
+        "email":email
+      });
+      if (res.statusCode == HttpStatus.ok) {
+        // NO NEEDED JUST TO KNOW IF IT RIGHT
+        return right(res.data['status']);
+      }
+      else {
+        return left(ServerFailure(message: res.data['message'].toString()));
+      }
+    } on DioException catch (e) {
+      return left(ServerFailure(message: e.message.toString()));
+    }
+  }
+}
