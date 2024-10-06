a834442532ed21549920d22979b2da5687811760
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-06 21:44:19 +0200

    added resetPasswordFailure class

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -8,3 +8,6 @@ abstract class Failure{
 class ServerFailure extends Failure{
   ServerFailure({required super.message});
 }
+class RestPasswordFailure extends Failure{
+  RestPasswordFailure({required super.message});
+}
