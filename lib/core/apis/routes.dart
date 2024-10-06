ebf1fbf20dca17f6fc896e4f443ce3ea895364fd
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-06 21:40:11 +0200

    added  USER_BY_TOKEN URL

--- lib/core/apis/routes.dart
+++ lib/core/apis/routes.dart
@@ -1,8 +1,9 @@
 
 
 abstract class Routes {
-  static const String BASE_URL = 'http://192.168.9.140:3000/api';
+  static const String BASE_URL = 'http://192.168.1.98:8000/api';
   static const String LOGIN_URL = '$BASE_URL/login';
   static const String REGISTER_URL = '$BASE_URL/register';
-  static const String USER_BY_TOKEN = '$BASE_URL/me';
+  static const String USER_BY_TOKEN_URL = '$BASE_URL/me';
+  static const String REST_PASSWORD_URL = '$BASE_URL/forget-password';
 }
