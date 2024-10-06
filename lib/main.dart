347b55c57389671d8574e31e545ccea17ecdad7e
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-06 21:49:01 +0200

    added onBoarding Check

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,50 @@
 import 'package:Basera/core/dependency_injection.dart';
 import 'package:Basera/core/utility/theme.dart';
 import 'package:Basera/features/Auth/presentation/manager/token/token_cubit.dart';
-import 'package:Basera/features/Auth/presentation/views/login_view.dart';
-import 'package:Basera/features/home/presentation/views/home_view.dart';
-import 'package:Basera/features/profile/presentation/views/profile_view.dart';
+import 'package:Basera/features/Auth/presentation/views/auth_guard.dart';
+import 'package:Basera/features/Auth/presentation/views/forget_password_view.dart';
+import 'package:Basera/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'core/utility/size_config.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
   setup();
+  var sp = await SharedPreferences.getInstance();
+  bool isFirstTime = !sp.containsKey('isFirstTime');
+  if (isFirstTime) {
+    await sp.setBool('isFirstTime', false);
+  }
+  runApp(MyApp(isFirstTime: isFirstTime));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isFirstTime;
+
+  const MyApp({super.key, required this.isFirstTime});
 
   @override
   Widget build(BuildContext context) {
-    final brightness = View
-        .of(context)
-        .platformDispatcher
-        .platformBrightness;
+    final brightness = View.of(context).platformDispatcher.platformBrightness;
 
     MaterialTheme theme = const MaterialTheme();
-    SizeConfig.init(context);
     return BlocProvider(
       create: (context) => TokenCubit(),
-      child: MaterialApp(
-        theme: brightness == Brightness.light ? theme.light() : theme.dark(),
-        debugShowCheckedModeBanner: false,
-        home: const ProfileView(),
-        builder: (context, child) {
-          return Directionality(
-            textDirection: TextDirection.rtl,
-            child: child!,
+      child: Builder(
+        builder: (context) {
+          SizeConfig.init(context);
+          return MaterialApp(
+            theme: brightness == Brightness.light ? theme.light() : theme.dark(),
+            debugShowCheckedModeBanner: false,
+            home: isFirstTime ? const OnBoardingView() : const AuthGuard(),
+            builder: (context, child) {
+              return Directionality(
+                textDirection: TextDirection.rtl,
+                child: child!,
+              );
+            },
           );
         },
       ),
