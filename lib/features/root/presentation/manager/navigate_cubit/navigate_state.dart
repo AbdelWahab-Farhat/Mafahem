5e342174e0e7b9f90440af26dae60f44463b3fd8
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-07 00:42:02 +0200

    root logic and design

---
+++ lib/features/root/presentation/manager/navigate_cubit/navigate_state.dart
@@ -0,0 +1,6 @@
+part of 'navigate_cubit.dart';
+
+@immutable
+sealed class NavigateState {}
+
+final class NavigateInitial extends NavigateState {}
--- lib/features/root/presentation/views/widgets/root_view_body.dart
+++ lib/features/root/presentation/views/widgets/root_view_body.dart
@@ -1,6 +1,3 @@
-import 'dart:developer';
-
-import 'package:Basera/features/Auth/presentation/manager/token/token_cubit.dart';
 import 'package:Basera/features/root/presentation/manager/navigate_cubit/navigate_cubit.dart';
 import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:flutter/material.dart';
