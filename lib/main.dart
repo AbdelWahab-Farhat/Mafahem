604b3ddf27487d0bf4a8834cbc1af4b9041ee6a9
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-08 00:46:47 +0200

    removed imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:Basera/core/dependency_injection.dart';
 import 'package:Basera/core/utility/theme.dart';
 import 'package:Basera/features/Auth/presentation/manager/token/token_cubit.dart';
-import 'package:Basera/features/Auth/presentation/views/auth_guard.dart';
-import 'package:Basera/features/Auth/presentation/views/forget_password_view.dart';
 import 'package:Basera/features/on_boarding/presentation/views/on_boarding_view.dart';
+import 'package:Basera/features/root/presentation/views/root_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -38,7 +37,7 @@ class MyApp extends StatelessWidget {
           return MaterialApp(
             theme: brightness == Brightness.light ? theme.light() : theme.dark(),
             debugShowCheckedModeBanner: false,
-            home: isFirstTime ? const OnBoardingView() : const AuthGuard(),
+            home: isFirstTime ? const OnBoardingView() : const RootView(),
             builder: (context, child) {
               return Directionality(
                 textDirection: TextDirection.rtl,
