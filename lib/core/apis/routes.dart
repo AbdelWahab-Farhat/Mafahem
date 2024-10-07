076d336ef98e41fe49c1551e04ba4a776aa8ecca
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-08 00:41:06 +0200

    added new route (road maps url)

--- lib/core/apis/routes.dart
+++ lib/core/apis/routes.dart
@@ -1,9 +1,10 @@
 
 
 abstract class Routes {
-  static const String BASE_URL = 'http://192.168.1.98:8000/api';
+  static const String BASE_URL = 'http://192.168.231.140:3000/api';
   static const String LOGIN_URL = '$BASE_URL/login';
   static const String REGISTER_URL = '$BASE_URL/register';
   static const String USER_BY_TOKEN_URL = '$BASE_URL/me';
   static const String REST_PASSWORD_URL = '$BASE_URL/forget-password';
+  static const String ROAD_MAPS_URL = "$BASE_URL/roadmaps";
 }
