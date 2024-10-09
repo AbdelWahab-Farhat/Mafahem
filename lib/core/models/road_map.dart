ab4cbf7bb749b0a333f6ae4c3b552ef15f1b0519
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-09 22:08:31 +0200

    changed some values to nullable

--- lib/core/models/road_map.dart
+++ lib/core/models/road_map.dart
@@ -4,29 +4,31 @@ class RoadMap {
   final int id;
   final String title;
   final String description;
-  final String createdAt;
-  final String updatedAt;
-  final double discountedPrice;
+  final String? createdAt;
+  final String? updatedAt;
+  final double? discountedPrice;
   final List<Course> courses;
-
+  final int? numberOfStudents ;
   RoadMap({
     required this.id,
     required this.title,
     required this.description,
-    required this.createdAt,
-    required this.updatedAt,
+     this.createdAt,
+     this.updatedAt,
     required this.discountedPrice,
     required this.courses,
+     this.numberOfStudents
   });
 
   factory RoadMap.fromJson(Map<String, dynamic> json) {
     return RoadMap(
       id: json['id'],
+      numberOfStudents:json['enrollments_count'],
       title: json['title'],
       description: json['description'],
       createdAt: json['created_at'],
       updatedAt: json['updated_at'],
-      discountedPrice: json['discounted_price'].toDouble(),
+      discountedPrice: json['discounted_price']?.toDouble(),
       courses: (json['courses'] as List).map((course) => Course.fromJson(course)).toList(),
     );
   }
@@ -40,6 +42,7 @@ class RoadMap {
       'updated_at': updatedAt,
       'discounted_price': discountedPrice,
       'courses': courses.map((course) => course.toJson()).toList(),
+      'enrollments_count':numberOfStudents
     };
   }
 }
