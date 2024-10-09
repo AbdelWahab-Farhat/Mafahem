adc03c5a80087fdb9fa86314879e3f6ea5d5551e
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-09 22:28:23 +0200

    changed the connectTimeout to 5s.

--- lib/core/dependency_injection.dart
+++ lib/core/dependency_injection.dart
@@ -1,13 +1,13 @@
+import 'package:Basera/features/Auth/presentation/manager/token/token_cubit.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-final getIt = GetIt.instance;
 
+final getIt = GetIt.instance;
 
 void setup() {
   Dio dio = Dio(
-    BaseOptions(
-
-    ),
+    BaseOptions(connectTimeout: const Duration(seconds: 5)),
   );
   getIt.registerSingleton<Dio>(dio);
+  getIt.registerSingleton<TokenCubit>(TokenCubit());
 }
