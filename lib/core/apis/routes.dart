f25b3564e6182d5998469d0c222a344b777c34ac
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-11 00:10:27 +0200

    new routes

--- lib/core/apis/routes.dart
+++ lib/core/apis/routes.dart
@@ -1,8 +1,8 @@
 
-
 abstract class Routes {
-  static const String BASE_URL = 'http://192.168.194.140:3000/api';
+  static const String BASE_URL = 'http://192.168.109.140:3000/api';
   static const String LOGIN_URL = '$BASE_URL/login';
+  static const String LOGOUT_URL = '$BASE_URL/logout';
   static const String REGISTER_URL = '$BASE_URL/register';
   static const String USER_BY_TOKEN_URL = '$BASE_URL/me';
   static const String REST_PASSWORD_URL = '$BASE_URL/forget-password';
@@ -11,4 +11,5 @@ abstract class Routes {
   static const String USER_COURSES_URL = "$BASE_URL/enroll/index";
   static const String USER_ROAD_MAPS = "$BASE_URL/roadmapss/enrollments";
   static const String USER_CERTIFICATE_URL = "$BASE_URL/courses/certificate/get";
+
 }
