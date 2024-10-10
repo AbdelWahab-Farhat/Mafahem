79b9ab821c0d5501415e9bed231a2d8c1aeb8afd
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-10 22:04:33 +0200

    added firebase notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,36 @@
+import 'dart:developer';
+
+import 'package:Basera/core/apis/fire_base_api.dart';
 import 'package:Basera/core/dependency_injection.dart';
 import 'package:Basera/core/utility/theme.dart';
 import 'package:Basera/features/Auth/presentation/manager/token/token_cubit.dart';
 import 'package:Basera/features/Auth/presentation/views/auth_guard.dart';
-import 'package:Basera/features/category/views/category_view.dart';
-import 'package:Basera/features/home/presentation/views/home_view.dart';
 import 'package:Basera/features/on_boarding/presentation/views/on_boarding_view.dart';
-import 'package:Basera/features/root/presentation/views/root_view.dart';
+import 'package:Basera/features/setting/presentation/manager/Theme_cubit/theme_cubit.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'core/utility/size_config.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setup();
+  await Firebase.initializeApp();
+
+  await   FireBaseApi().initNotifications();
+
   var sp = await SharedPreferences.getInstance();
+  log(sp.getString('token') ?? 'no token');
   bool isFirstTime = !sp.containsKey('isFirstTime');
   if (isFirstTime) {
     await sp.setBool('isFirstTime', false);
   }
-  runApp(MyApp(isFirstTime: isFirstTime));
+  runApp(BlocProvider(
+    create: (context) => ThemeCubit(),
+    child: MyApp(isFirstTime: isFirstTime),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -29,22 +40,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final brightness = View.of(context).platformDispatcher.platformBrightness;
+    var themeCubit = context.read<ThemeCubit>();
 
     MaterialTheme theme = const MaterialTheme();
     return BlocProvider(
-      create: (context) => TokenCubit(),
+      create: (context) => GetIt.instance.get<TokenCubit>(),
       child: Builder(
         builder: (context) {
           SizeConfig.init(context);
-          return MaterialApp(
-            theme: brightness == Brightness.light ? theme.light() : theme.dark(),
-            debugShowCheckedModeBanner: false,
-            home: isFirstTime ? const OnBoardingView() : const RootView(),
-            builder: (context, child) {
-              return Directionality(
-                textDirection: TextDirection.rtl,
-                child: child!,
+          return BlocBuilder<ThemeCubit, ThemeState>(
+            builder: (context, state) {
+              return MaterialApp(
+                theme: themeCubit.isDark ? theme.dark() : theme.light(),
+                debugShowCheckedModeBanner: false,
+                home: isFirstTime ? const OnBoardingView() : const AuthGuard(),
+                builder: (context, child) {
+                  return Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: child!,
+                  );
+                },
               );
             },
           );
