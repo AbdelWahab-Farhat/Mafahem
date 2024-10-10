d0b31f95f3a643a1a3f77f4ce22961374c7612b3
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-11 00:06:55 +0200

    CHANGE THE METHOD NAME TO GET USER ITEMS 'OOPS' and added certificate method

--- lib/features/profile/presentation/manager/user_cubit/user_cubit.dart
+++ lib/features/profile/presentation/manager/user_cubit/user_cubit.dart
@@ -1,6 +1,8 @@
 
 import 'package:Basera/core/models/course.dart';
 import 'package:Basera/core/models/road_map.dart';
+import 'package:Basera/features/Auth/data/certificate.dart';
+import 'package:Basera/features/profile/data/fetch_user_certificate_service.dart';
 import 'package:Basera/features/profile/data/fetch_user_courses_service.dart';
 import 'package:Basera/features/profile/data/fetch_user_road_maps_service.dart';
 import 'package:bloc/bloc.dart';
@@ -11,32 +13,37 @@ part 'user_state.dart';
 class UserCubit extends Cubit<UserState> {
   List<Course> userCourses = [];
   List<RoadMap> userRoadMaps = [];
+  List<Certificate> userCertificates = [];
 
   UserCubit() : super(UserLoading());
 
-  Future<void> getUserCourses() async {
-    emit(UserLoading()); // Emit loading state
+  Future<void> getUserItems() async {
+    emit(UserLoading());
 
     var result = await Future.wait([
       FetchUserCoursesService.getUserCourses(),
       FetchUserRoadMapsService.getUserRoadMaps(),
+      FetchUserCertificateService.getUserCertificate()
     ]);
 
-    // Handle the result from both services
     result[0].fold(
           (failure) => emit(UserFailure(errMessage: failure.message)), // Handle error for courses
           (courses) {
         userCourses = courses as List<Course>; // Update courses if successful
-        result[1].fold(
-              (failure) => emit(UserFailure(errMessage: failure.message)), // Handle error for roadmaps
-              (roadMaps) {
-            userRoadMaps = roadMaps as List<RoadMap>; // Update roadmaps if successful
-            // Emit success state after both courses and roadmaps are fetched
-            emit(UserSuccess());
-          },
-        );
       },
     );
+    result[1].fold(
+          (failure) => emit(UserFailure(errMessage: failure.message)), // Handle error for roadmaps
+          (roadMaps) {
+        userRoadMaps = roadMaps as List<RoadMap>; // Update roadmaps if successful
+      },
+    );
+    result[2].fold((failure) => emit(UserFailure(errMessage: failure.message)), // Handle error for roadmaps
+          (certificates) {
+        userCertificates = certificates as List<Certificate>;
+      },
+    );
+    emit(UserSuccess());
   }
 }
 
