2e51a38d8f015376de7dcb6791185895bdfb0a46
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-11 00:50:53 +0200

    added       crossAxisAlignment: CrossAxisAlignment.start,  to column

--- lib/features/setting/presentation/views/widgets/more_section.dart
+++ lib/features/setting/presentation/views/widgets/more_section.dart
@@ -1,3 +1,5 @@
+import 'package:Basera/core/utility/functions/navigate_functions.dart';
+import 'package:Basera/features/setting/presentation/views/terms_and_conditions_view.dart';
 import 'package:Basera/features/setting/presentation/views/widgets/setting_list_tile.dart';
 import 'package:flutter/material.dart';
 
@@ -9,8 +11,10 @@ class MoreSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         SettingListTile(
+          onTap: () => push(context, TermsAndConditionsView()),
           title: 'سياسة الخصوصية',
           trailing: Icon(
             Icons.arrow_forward_ios,
