63d4f8cfef61d18e38cecc482590e2e43e906b45
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-11 00:07:16 +0200

    added shimmer and changed some stuffs

--- lib/features/profile/presentation/views/widgets/user_image_with_name.dart
+++ lib/features/profile/presentation/views/widgets/user_image_with_name.dart
@@ -1,12 +1,14 @@
 import 'dart:developer';
 
+import 'package:Basera/core/utility/size_config.dart';
 import 'package:Basera/core/utility/styles.dart';
-import 'package:Basera/core/widgets/custom_loading_widget.dart';
+import 'package:Basera/core/widgets/custom_avatar_widget.dart';
 import 'package:Basera/features/Auth/presentation/manager/token/token_cubit.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shimmer/shimmer.dart';
 
 class UserImageWithName extends StatelessWidget {
   const UserImageWithName({
@@ -16,29 +18,16 @@ class UserImageWithName extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var tokenCubit = context.read<TokenCubit>();
-    log(tokenCubit.user!.data!.profilePhotoUrl.toString());
+    log(tokenCubit.user!.data.profilePhotoUrl.toString());
 
     return Column(
       children: [
-        CircleAvatar(
-          radius: 37.5,
-          backgroundColor: Colors.grey.shade200,
-          child: ClipOval(
-            child: SvgPicture.network(
-              tokenCubit.user!.data!.profilePhotoUrl!,
-              placeholderBuilder: (context) => const CustomLoadingWidget(),
-              height: double.infinity,
-              width: double.infinity,
-              fit: BoxFit.cover,
-              semanticsLabel: 'Profile image of ${tokenCubit.user!.data!.name}', // Accessibility
-            ),
-          ),
-        ),
+        const CustomAvatarWidget(),
         const SizedBox(
           height: 8,
         ),
         Text(
-          tokenCubit.user!.data!.name ?? "User",
+          tokenCubit.user!.data.name ?? "User",
           style: Styles.style20(context).copyWith(
             color: Theme.of(context).colorScheme.onSurface,
             fontFamily: GoogleFonts.changa().fontFamily,
@@ -48,3 +37,4 @@ class UserImageWithName extends StatelessWidget {
     );
   }
 }
+
