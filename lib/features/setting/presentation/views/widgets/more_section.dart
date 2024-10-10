ba310e7606ae76ce95414853d80be6adc58fa039
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-11 00:01:19 +0200

    setting design part

---
+++ lib/features/setting/presentation/views/widgets/more_section.dart
@@ -0,0 +1,40 @@
+import 'package:Basera/features/setting/presentation/views/widgets/setting_list_tile.dart';
+import 'package:flutter/material.dart';
+
+class MoreSection extends StatelessWidget {
+  const MoreSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SettingListTile(
+          title: 'سياسة الخصوصية',
+          trailing: Icon(
+            Icons.arrow_forward_ios,
+            color: Theme.of(context).colorScheme.onSurface,
+            size: 16,
+          ),
+        ),
+        SettingListTile(
+          title: 'اشترك في البريميوم',
+          trailing: Icon(
+            Icons.arrow_forward_ios,
+            color: Theme.of(context).colorScheme.onSurface,
+            size: 16,
+          ),
+        ),
+        SettingListTile(
+          title: 'كن من طاقمنا التعليمي',
+          trailing: Icon(
+            Icons.arrow_forward_ios,
+            color: Theme.of(context).colorScheme.onSurface,
+            size: 16,
+          ),
+        ),
+      ],
+    );
+  }
+}
