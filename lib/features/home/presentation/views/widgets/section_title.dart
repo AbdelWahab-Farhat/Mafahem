499eeb7cbf0c055689e886c4e8f66a7a5ec45d1a
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-12 15:09:36 +0200

    bring "المزيد" again.

--- lib/features/home/presentation/views/widgets/section_title.dart
+++ lib/features/home/presentation/views/widgets/section_title.dart
@@ -3,22 +3,33 @@ import 'package:flutter/material.dart';
 
 class SectionTitle extends StatelessWidget {
   final String title;
+  final bool hasMore;
   const SectionTitle({
     super.key, required this.title,
+    this.hasMore = true,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          title,
-          style: Styles.style16(context)
-              .copyWith(fontWeight: FontWeight.w600 , color: Theme.of(context).colorScheme.primary),
-        ),
-      ],
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: hasMore ? 20 : 0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: Styles.style16(context)
+                .copyWith(fontWeight: FontWeight.w600),
+          ),
+          if (hasMore)
+          Text(
+            'المزيد...',
+            style: Styles.style14(context)
+                .copyWith(color: Theme.of(context).colorScheme.tertiary),
+          )
+        ],
+      ),
     );
   }
 }
