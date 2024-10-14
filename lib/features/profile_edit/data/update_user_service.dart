109f668f2e6375724c2824083c125d2aeec4a14f
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-14 12:20:18 +0200

    update user method

---
+++ lib/features/profile_edit/data/update_user_service.dart
@@ -0,0 +1,7 @@
+import 'package:Basera/core/error/failure.dart';
+import 'package:Basera/core/models/user.dart';
+import 'package:Basera/features/Auth/presentation/manager/token/token_cubit.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
