6fe881866951eb96ec396240aaa14e650c0d3724
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-14 12:20:41 +0200

    added phone validate and multilines validate

--- lib/core/utility/functions/validate_functions.dart
+++ lib/core/utility/functions/validate_functions.dart
@@ -39,3 +39,27 @@ String? validateUsername(String? username) {
   return null; // Valid username
 }
 
+String? validateLibyanPhoneNumber(String? phoneNumber) {
+  // Check if the phone number is null or empty
+  if (phoneNumber == null || phoneNumber.isEmpty) {
+    return 'لا يجب أن يكون رقم الهاتف فارغاً'; // "Phone number cannot be empty"
+  }
+  // Check if the phone number matches the Libyan format
+  else if (!RegExp(r'^0(91|92|93|94|95|96|97|98|99)\d{7}$').hasMatch(phoneNumber)) {
+    return 'رقم الهاتف غير صحيح. يجب أن يبدأ ب 0 ويتبع برمز المنطقة ثم 7 أرقام'; // "Invalid phone number. It must start with 0, followed by an area code and then 7 digits"
+  }
+  return null; // Valid phone number
+}
+
+String? validateMultiLineInput(String? input) {
+  if (input == null || input.isEmpty) {
+    return 'لا يجب أن يكون الحقل فارغاً';
+  }
+
+  List<String> lines = input.split('\n');
+  if (lines.length < 3) {
+    return 'يجب أن تحتوي المدخلات على 3 سطور على الأقل';
+  }
+
+  return null;
+}
---
+++ lib/features/profile_edit/data/take_photo_service.dart
@@ -0,0 +1,21 @@
+import 'package:image_picker/image_picker.dart';
+
+class TakePhotoService {
+  static final ImagePicker _picker = ImagePicker();
+
+  static Future<String?> takePhoto() async {
+    try {
+      // Pick the image from the gallery
+      final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+
+      if (image != null) {
+        return image.path;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Error picking image: $e');
+      return null;
+    }
+  }
+}
