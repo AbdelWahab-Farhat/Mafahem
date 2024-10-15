74272f0e8cebece0e115d81c23e870dccf6d25ea
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-15 18:55:59 +0200

    added info from course to it (dynamic) and not finished yet

--- lib/features/Course/presentation/views/about_section.dart
+++ lib/features/Course/presentation/views/about_section.dart
@@ -1,13 +1,22 @@
+
+import 'package:Basera/core/models/course.dart';
 import 'package:Basera/core/utility/size_config.dart';
 import 'package:Basera/core/utility/styles.dart';
 import 'package:Basera/core/widgets/custom_filled_button.dart';
+import 'package:Basera/core/widgets/custom_textfield.dart';
+import 'package:Basera/features/Course/data/cal_user_rate_service.dart';
+import 'package:Basera/features/Course/presentation/manager/review_cubit/review_cubit.dart';
 import 'package:Basera/features/Course/presentation/views/widgets/about_course_text_icon.dart';
+import 'package:Basera/features/Course/presentation/views/widgets/comment_rate_list_view.dart';
 import 'package:Basera/features/Course/presentation/views/widgets/courseRating.dart';
 import 'package:Basera/features/Course/presentation/views/widgets/mini_section.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class AboutSection extends StatelessWidget {
-  const AboutSection({super.key});
+  final Course course;
+  const AboutSection({super.key, required this.course});
 
   @override
   Widget build(BuildContext context) {
@@ -17,39 +26,37 @@ class AboutSection extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const AboutCourseTextIcon(
-              title: '42 ساعة',
+            AboutCourseTextIcon(
+              title: '${(course.duration / 60).toStringAsFixed(0)} ساعات',
               icon: Icons.timer_outlined,
             ),
             const SizedBox(
               height: 16,
             ),
-            const AboutCourseTextIcon(
-              title: 'شهادة أكمال الجنس',
+            AboutCourseTextIcon(
+              title: 'شهادة ${course.title}',
               icon: Icons.celebration_rounded,
             ),
             const SizedBox(
               height: 16,
             ),
-            const AboutCourseTextIcon(
-              title: 'للمبتدئين',
+            AboutCourseTextIcon(
+              title: course.level,
               icon: Icons.network_cell_outlined,
             ),
             const SizedBox(
               height: 24,
             ),
-            const MiniSection(
+            MiniSection(
               title: 'ماذا سوف تتعلم',
-              content:
-                  'برنامج أساسيات تعلم الآلة مصمم لتقديم أساس قوي ومهارات جاهزة للعمل لمهندسي تعلم الآلة. برنامج أساسيات تعلم الآلة مصمم لتقديم أساس قوي ومهارات جاهزة للعمل لمهندسي تعلم الآلة. اقرأ المزيد.',
+              content: course.description,
             ),
             const SizedBox(
               height: 16,
             ),
-            const MiniSection(
+            MiniSection(
               title: 'المتطلبات',
-              content:
-                  'برنامج أساسيات تعلم الآلة مصمم لتقديم أساس قوي ومهارات جاهزة للعمل لمهندسي تعلم الآلة. برنامج أساسيات تعلم الآلة مصمم لتقديم أساس قوي ومهارات جاهزة للعمل لمهندسي تعلم الآلة. اقرأ المزيد.',
+              content: course.requirements ?? 'لا يوجد متطلبات لهذا الكورس ',
             ),
             const SizedBox(
               height: 24,
@@ -61,36 +68,41 @@ class AboutSection extends StatelessWidget {
             const SizedBox(
               height: 4,
             ),
-            const CourseRatings(
-              percentageByStar: [1, 0.5, 0.1, 0.4, 0.3],
+            CourseRatings(
+              percentageByStar: [
+                CalUserRateService.presentUserRateByStars(course, 5),
+                CalUserRateService.presentUserRateByStars(course, 4),
+                CalUserRateService.presentUserRateByStars(course, 3),
+                CalUserRateService.presentUserRateByStars(course, 2),
+                CalUserRateService.presentUserRateByStars(course, 1),
+              ],
             ),
             const SizedBox(
               height: 16,
             ),
-            const CommentWidget(),
-            Divider(
-              color: Theme.of(context).colorScheme.onSurface.withOpacity(0.2),
-              thickness: 0.4,
-            ),
-            const CommentWidget(),
+            CommentRateListView(course: course),
             const SizedBox(
               height: 24,
             ),
-            CustomFilledButton(
-              onPressed: () {},
-              title: 'عرض المزيد',
-              width: SizeConfig.screenWidth,
-              color: Theme.of(context).colorScheme.primary,
-              textColor: Theme.of(context).colorScheme.onPrimary,
-              fontSize: 18,
-              radius: 8,
-              height: 45,
-            ),
+            course.raters!.isNotEmpty && course.raters!.length > 4
+                ? CustomFilledButton(
+                    onPressed: () {},
+                    title: 'عرض المزيد',
+                    width: SizeConfig.screenWidth,
+                    color: Theme.of(context).colorScheme.primary,
+                    textColor: Theme.of(context).colorScheme.onPrimary,
+                    fontSize: 18,
+                    radius: 8,
+                    height: 45,
+                  )
+                : const SizedBox(),
             const SizedBox(
-              height: 17,
+              height: 8,
             ),
             CustomFilledButton(
-              onPressed: () {},
+              onPressed: () {
+                showReviewDialog(context);
+              },
               title: 'اضافة مراجعة',
               width: SizeConfig.screenWidth,
               color: Theme.of(context).colorScheme.primary,
@@ -109,40 +121,67 @@ class AboutSection extends StatelessWidget {
   }
 }
 
-class CommentWidget extends StatelessWidget {
-  // obj of course comment
-  const CommentWidget({
-    super.key,
-  });
+void showReviewDialog(BuildContext context) {
+  ReviewCubit reviewCubit = context.read<ReviewCubit>();
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'بكر بكر',
-          style: Styles.style12(context).copyWith(fontWeight: FontWeight.w600),
-        ),
-        Text(
-          '2024/9/2',
-          style: Styles.style12(context),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: List.generate(
-            5,
-            (index) => Icon(
-              index < 3 ? Icons.star : Icons.star_border,
-              color: Colors.amber,
-              size: 16,
-            ),
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return BlocProvider<ReviewCubit>.value(
+        value: reviewCubit,
+        child: AlertDialog(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
           ),
+          title: Text("تقييم الكورس",
+              textAlign: TextAlign.center,
+              style: Styles.style20(context).copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              )),
+          content: BlocBuilder<ReviewCubit, ReviewState>(
+            builder: (context, state) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  RatingBar.builder(
+                    initialRating: reviewCubit.userRating,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: false,
+                    itemCount: 5,
+                    itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                    itemBuilder: (context, _) => const Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    onRatingUpdate: reviewCubit.onRatingUpdate,
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    reviewCubit.ratingMessage,
+                    style: Styles.style18(context).copyWith(
+                        color: Theme.of(context).colorScheme.onSurface),
+                  ),
+                  const SizedBox(height: 20),
+                  CustomTextField(
+                    controller: reviewCubit.commentController,
+                    maxLines: 5,
+                  ),
+                ],
+              );
+            },
+          ),
+          actions: [
+            CustomFilledButton(
+              title: 'أرسال',
+              onPressed: () {},
+              textColor: Theme.of(context).colorScheme.onPrimary,
+              radius: 36,
+            ),
+          ],
         ),
-        const Text(
-            'برنامج أساسيات تعلم الآلة مصمم لتقديم أساس قوي ومهارات جاهزة للعمل لمهندسي تعلم الآلة. برنامج أساسيات تعلم الآلة مصمم لتقديم أساس قوي ومهارات جاهزة للعمل لمهندسي تعلم الآلة. اقرأ المزيد.'),
-      ],
-    );
-  }
+      );
+    },
+  );
 }
