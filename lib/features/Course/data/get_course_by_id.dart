89df17a3d17a259eaf9e0f75fb7679f8a2a701b4
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-16 22:37:09 +0200

    to get the fucking course by id but not completed yet

---
+++ lib/features/Course/data/get_course_by_id.dart
@@ -0,0 +1,35 @@
+
+
+
+
+
+import 'package:Basera/core/apis/routes.dart';
+import 'package:Basera/core/error/failure.dart';
+import 'package:Basera/core/models/course.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+// later we complete that
+//TODO FINISH IT WITH REFREACH 
+class GetCourseByIdService {
+  static Future<Either<Failure, Course>> getCourseById(String id) async {
+    try {
+      final response = await Dio().get(
+        '${Routes.GET_COURSE_BY_ID_URL}/$id',
+        options: Options(
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        return right(Course.fromJson(response.data));
+      } else {
+        return left(ServerFailure(message: response.data['message']));
+      }
+    } on DioException catch (e) {
+      return left(ServerFailure(message: e.response?.data['message'] ?? 'error  occurred'));
+    }
+  }
+}
