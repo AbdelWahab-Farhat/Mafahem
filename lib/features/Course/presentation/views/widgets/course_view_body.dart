704cffc506c4e5807134060fc407fe7d906b3df9
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-16 15:39:03 +0200

    added logic to about (not finished)

--- lib/features/Course/presentation/views/widgets/course_view_body.dart
+++ lib/features/Course/presentation/views/widgets/course_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:Basera/core/models/course.dart';
 import 'package:Basera/core/utility/size_config.dart';
 import 'package:Basera/core/utility/styles.dart';
 import 'package:Basera/features/Course/presentation/views/about_section.dart';
@@ -5,10 +6,12 @@ import 'package:Basera/features/Course/presentation/views/lecture_section.dart';
 import 'package:Basera/features/Course/presentation/views/similar_courses_section.dart';
 import 'package:Basera/features/Course/presentation/views/widgets/add_to_cart_nav_bottom.dart';
 import 'package:Basera/features/Course/presentation/views/widgets/header_section.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CourseViewBody extends StatelessWidget {
-  const CourseViewBody({super.key});
+  final Course course;
+  const CourseViewBody({super.key, required this.course});
 
   @override
   Widget build(BuildContext context) {
@@ -22,16 +25,14 @@ class CourseViewBody extends StatelessWidget {
             SizedBox(
                 width: SizeConfig.screenWidth,
                 height: SizeConfig.screenHeight * 0.27,
-                child: Image.asset(
-                  'lib/assets/images/course-image.png',
-                  fit: BoxFit.cover,
-                )),
+                child: CachedNetworkImage(imageUrl: course.image,fit: BoxFit.fitWidth,)
+            ),
             Padding(
               padding: const EdgeInsets.only(right: 20, left: 20, top: 16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const HeaderSection(),
+                   HeaderSection(course:course),
                   const SizedBox(height: 20),
                   TabBar(
                     tabs: const [
@@ -41,31 +42,34 @@ class CourseViewBody extends StatelessWidget {
                     ],
                     labelStyle: Styles.style12(context).copyWith(fontSize: 13),
                     splashFactory:
-                    InkSparkle.constantTurbulenceSeedSplashFactory,
+                        InkSparkle.constantTurbulenceSeedSplashFactory,
                     indicatorWeight: 2,
                     labelPadding: EdgeInsets.zero,
                     dividerHeight: 0.2,
                     dividerColor: Theme.of(context).colorScheme.secondary,
                     labelColor: Theme.of(context).colorScheme.primary,
-                    unselectedLabelColor:
-                    Theme.of(context).colorScheme.onSurface, // Inactive tab color
-                    indicatorColor: Theme.of(context).colorScheme.primary, // Tab indicator color
+                    unselectedLabelColor: Theme.of(context)
+                        .colorScheme
+                        .onSurface, // Inactive tab color
+                    indicatorColor: Theme.of(context)
+                        .colorScheme
+                        .primary, // Tab indicator color
                   ),
                 ],
               ),
             ),
-            const Expanded(
+             Expanded(
               child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
+                padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: TabBarView(
-                  physics: NeverScrollableScrollPhysics(),
+                  physics: const NeverScrollableScrollPhysics(),
                   children: [
                     // Tab 1 content with scroll
-                    AboutSection(),
+                    AboutSection(course: course,),
                     // Tab 2 content with scroll
-                    LectureSection(),
+                    const LectureSection(),
                     // Tab 3 content with scroll
-                    SimilarCoursesSection()
+                    const SimilarCoursesSection()
                   ],
                 ),
               ),
