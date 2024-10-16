ac774c32a69fef7c3240ed1fe3bf742f658f8010
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-16 15:27:48 +0200

    usecase for courses bill

---
+++ lib/features/cart/data/bill_service.dart
@@ -0,0 +1,19 @@
+import 'package:Basera/core/models/course.dart';
+
+class BillService{
+    static int numberOfCourses(List<Course> courses){
+      return courses.length;
+    }
+
+    static double totalPrice(List<Course> courses){
+      double total = 0;
+      for (var course in courses) {
+        total += double.parse(course.price);
+      }
+      return total;
+    }
+
+    static double discountedPrice(double totalPrice , double couponDiscountPrice){
+      return totalPrice - (totalPrice * (couponDiscountPrice / 100));
+    }
+}
