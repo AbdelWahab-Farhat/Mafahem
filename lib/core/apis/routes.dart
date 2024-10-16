c081db2fe60cc778fcbe991c27f896ccc24e3245
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-16 15:40:39 +0200

    new routes { tatic const String USER_CART_URL = "$BASE_URL/carts";   static const String APPLY_COUPON_URL = "$BASE_URL/coupons/by/code";   static const String REMOVE_FROM_CART = "$BASE_URL/carts/remove/item";   static const String ORDER_URL = "$BASE_URL/orders";   static const String PROMO_COURSES_URL ="$BASE_URL/courses/random/get";   static const String RATE_COURSES_URL ="$BASE_URL/courses/rate"; }

--- lib/core/apis/routes.dart
+++ lib/core/apis/routes.dart
@@ -1,5 +1,5 @@
 abstract class Routes {
-  static const String BASE_URL = 'http://192.168.1.98:8000/api';
+  static const String BASE_URL = 'http://192.168.10.140:3000/api';
   static const String LOGIN_URL = '$BASE_URL/login';
   static const String LOGOUT_URL = '$BASE_URL/logout';
   static const String REGISTER_URL = '$BASE_URL/register';
@@ -10,4 +10,11 @@ abstract class Routes {
   static const String USER_COURSES_URL = "$BASE_URL/enroll/index";
   static const String USER_ROAD_MAPS = "$BASE_URL/roadmapss/enrollments";
   static const String USER_CERTIFICATE_URL = "$BASE_URL/courses/certificate/get";
+  static const String USER_CART_URL = "$BASE_URL/carts";
+  static const String APPLY_COUPON_URL = "$BASE_URL/coupons/by/code";
+  static const String REMOVE_FROM_CART = "$BASE_URL/carts/remove/item";
+  static const String ORDER_URL = "$BASE_URL/orders";
+  static const String PROMO_COURSES_URL ="$BASE_URL/courses/random/get";
+  static const String RATE_COURSES_URL ="$BASE_URL/courses/rate";
+  static const String COURSE_REVIEW_URL ="$BASE_URL/courses/make/reviews";
 }
