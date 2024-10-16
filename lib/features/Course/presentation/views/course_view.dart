b1350383e2426a8cd1665795a7cdc26a0399d57a
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-16 15:38:28 +0200

    added blocs added cubits

--- lib/features/Course/presentation/views/course_view.dart
+++ lib/features/Course/presentation/views/course_view.dart
@@ -1,11 +1,19 @@
+import 'package:Basera/core/models/course.dart';
+import 'package:Basera/features/Course/presentation/manager/review_cubit/review_cubit.dart';
 import 'package:Basera/features/Course/presentation/views/widgets/course_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CourseView extends StatelessWidget {
-  const CourseView({super.key});
+  final Course course;
+
+  const CourseView({super.key, required this.course});
 
   @override
   Widget build(BuildContext context) {
-    return const CourseViewBody();
+    return BlocProvider(
+      create: (context) => ReviewCubit(),
+      child: CourseViewBody(course: course,),
+    );
   }
 }
