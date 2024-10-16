a5fdc25f9753feaf2117c73530869cef962c83b7
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-16 22:33:19 +0200

    not complete it but to show comments (review and rate)

--- lib/features/Course/presentation/views/widgets/comment_rate_list_view.dart
+++ lib/features/Course/presentation/views/widgets/comment_rate_list_view.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:Basera/core/models/course.dart';
+import 'package:Basera/features/Course/presentation/manager/comment_cubit/comment_cubit.dart';
 import 'package:Basera/features/Course/presentation/views/widgets/comment_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CommentRateListView extends StatelessWidget {
   const CommentRateListView({
@@ -15,18 +19,29 @@ class CommentRateListView extends StatelessWidget {
     if (course.raters != null || course.raters!.isEmpty) {
       return const SizedBox();
     }
-    return ListView.builder(
-        itemCount: 4,
-        itemBuilder: (BuildContext context, int index) {
-          return Column(
-            children: [
-              CommentWidget(course: course),
-              Divider(
-                color: Theme.of(context).colorScheme.onSurface.withOpacity(0.2),
-                thickness: 0.4,
-              ),
-            ],
-          );
-        });
+    log(course.raters.toString());
+    return BlocBuilder<CommentCubit, CommentState>(
+      builder: (context, state) {
+        if (state is CommentFailure) {
+
+        }
+        return ListView.builder(
+            itemCount: 4,
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                children: [
+                  CommentWidget(course: course),
+                  Divider(
+                    color: Theme.of(context)
+                        .colorScheme
+                        .onSurface
+                        .withOpacity(0.2),
+                    thickness: 0.4,
+                  ),
+                ],
+              );
+            });
+      },
+    );
   }
 }
