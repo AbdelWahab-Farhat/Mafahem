1792c56164d33ce2bc5974e3f748144fcf135cc7
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-16 15:27:25 +0200

    completed bill section with logic

--- lib/features/cart/presentation/views/widgets/bill_section.dart
+++ lib/features/cart/presentation/views/widgets/bill_section.dart
@@ -1,5 +1,9 @@
+import 'package:Basera/core/utility/functions/ui_functions.dart';
+import 'package:Basera/features/cart/presentation/manager/cart_cubit/cart_cubit.dart';
+import 'package:Basera/features/cart/presentation/manager/coupon_cubit/coupon_cubit.dart';
 import 'package:Basera/features/cart/presentation/views/widgets/bill_widget_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BillSection extends StatelessWidget {
   const BillSection({
@@ -8,6 +12,8 @@ class BillSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var cartCubit = context.read<CartCubit>();
+    var couponCubit = context.read<CouponCubit>();
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 20),
       decoration: BoxDecoration(
@@ -22,25 +28,47 @@ class BillSection extends StatelessWidget {
           ),
         ],
       ),
-      child: const Column(
-        children: [
-          BillWidgetItem(
-            title: 'العدد',
-            trailing: '3 كورسات',
-          ),
-          BillWidgetItem(
-            title: 'الخصم',
-            trailing: '0 د.ل',
-          ),
-          BillWidgetItem(
-            title: 'السعر الأصلي',
-            trailing: '100 د.ل',
-          ),
-          BillWidgetItem(
-            title: 'الأجمالي',
-            trailing: '100 د.ل',
-          ),
-        ],
+      child: BlocBuilder<CartCubit, CartState>(
+        builder: (context, state) {
+          return BlocConsumer<CouponCubit, CouponState>(
+            listener: (context, state) {
+              if (state is CouponSuccess) {
+                showCustomSnackBar(context, state.couponMessage);
+              } else if (state is CouponFailure) {
+                showCustomSnackBar(context, state.errMessage);
+              } else if (state is CouponLoading) {
+                showCustomSnackBar(context, 'يرجى الانتظار...');
+              }
+            },
+            builder: (context, state) {
+              return Column(
+                children: [
+                  BillWidgetItem(
+                    title: 'العدد',
+                    trailing:
+                        '${cartCubit.numberOfCourses().toString()} كورسات',
+                  ),
+                  BillWidgetItem(
+                    title: 'الخصم',
+                    trailing: couponCubit.coupon == null
+                        ? '0%'
+                        : '${couponCubit.coupon!.discountPercentage}%',
+                  ),
+                  BillWidgetItem(
+                    title: 'السعر الأصلي',
+                    trailing: '${cartCubit.totalPrice()} د.ل',
+                  ),
+                  BillWidgetItem(
+                    title: 'الأجمالي',
+                    trailing: couponCubit.coupon == null
+                        ? '${cartCubit.totalPrice()} د.ل'
+                        : '${cartCubit.discountedPrice(double.parse(couponCubit.coupon!.discountPercentage))}د.ل',
+                  ),
+                ],
+              );
+            },
+          );
+        },
       ),
     );
   }
