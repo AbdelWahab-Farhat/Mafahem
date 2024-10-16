869e2f8abe9c4452e105793da7fefe9db9bacfdc
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-16 15:37:18 +0200

    handling cart operations

---
+++ lib/features/cart/presentation/manager/cart_cubit/cart_cubit.dart
@@ -0,0 +1,50 @@
+
+import 'package:Basera/core/models/cart.dart';
+import 'package:Basera/features/cart/data/bill_service.dart';
+import 'package:Basera/features/cart/data/get_user_cart_service.dart';
+import 'package:Basera/features/cart/data/remove_from_user_cart_service.dart';
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'cart_state.dart';
+
+class CartCubit extends Cubit<CartState> {
+  Cart? cart;
+  CartCubit() : super(CartInitial());
+
+  Future<void> getUserCart() async {
+    emit(CartLoading());
+    var result = await GetUserCartService.getUserCart();
+    result.fold(
+      (l) => emit(CartFailure(errMessage: l.message)),
+      (r) {
+        cart = r;
+        emit(CartSuccess());
+      },
+    );
+  }
+  int numberOfCourses() {
+    return BillService.numberOfCourses(cart?.courses ?? []);
+  }
+
+  double totalPrice() {
+    return BillService.totalPrice(cart?.courses! ?? []);
+  }
+
+  double discountedPrice(double couponDiscountPrice) {
+    return BillService.discountedPrice(totalPrice(), couponDiscountPrice);
+  }
+
+  Future<void> removeFromCart(String courseID) async {
+    var result = await RemoveFromUserCartService.removeFromCart(courseID);
+    result.fold(
+      (l) => emit(CartFailure(errMessage: l.message)),
+      (r) {
+        cart!.courses!.removeWhere((element) => element.id.toString() == courseID);
+        emit(CartSuccess());
+      },
+    );
+  }
+
+
+}
---
+++ lib/features/cart/presentation/manager/cart_cubit/cart_state.dart
@@ -0,0 +1,12 @@
+part of 'cart_cubit.dart';
+
+@immutable
+sealed class CartState {}
+
+final class CartInitial extends CartState {}
+final class CartLoading extends CartState {}
+final class CartFailure extends CartState {
+  final String errMessage;
+  CartFailure({required this.errMessage});
+}
+final class CartSuccess extends CartState {}
