695545d0ec191e0f492136629f23c1b665786de9
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-16 15:28:02 +0200

    usecase for rates

---
+++ lib/features/Course/data/cal_user_rate_service.dart
@@ -0,0 +1,31 @@
+import 'dart:developer';
+
+import 'package:Basera/core/models/course.dart';
+import 'package:Basera/core/models/rater.dart';
+
+class CalUserRateService {
+   static int _numberOfUserByStars(Course course, starNumber) {
+    int numberOfUser = 0;
+    List<Rater> raters = course.raters ?? [];
+    if (raters.isNotEmpty) {
+      for (var rater in raters) {
+        if (starNumber == rater.pivot.rating) {
+          numberOfUser++;
+        }
+      }
+    }
+    return numberOfUser;
+  }
+
+  static double presentUserRateByStars(Course course, starNumber) {
+    int numberOfUser = _numberOfUserByStars(course, starNumber);
+    log(course.raters.toString());
+    if (course.raters!.isNotEmpty) {
+      log('testing');
+      return numberOfUser / course.raters!.length;
+    }
+    else {
+      return 0;
+    }
+  }
+}
