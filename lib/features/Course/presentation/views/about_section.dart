54e64a499ddf0e0398a517890909f376e59a4eca
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-16 22:35:00 +0200

    added something useless

--- lib/features/Course/presentation/views/about_section.dart
+++ lib/features/Course/presentation/views/about_section.dart
@@ -1,11 +1,13 @@
-
 import 'package:Basera/core/models/course.dart';
+import 'package:Basera/core/utility/functions/ui_functions.dart';
 import 'package:Basera/core/utility/size_config.dart';
 import 'package:Basera/core/utility/styles.dart';
 import 'package:Basera/core/widgets/custom_filled_button.dart';
 import 'package:Basera/core/widgets/custom_textfield.dart';
 import 'package:Basera/features/Course/data/cal_user_rate_service.dart';
+import 'package:Basera/features/Course/data/show_review_dialog.dart';
 import 'package:Basera/features/Course/presentation/manager/review_cubit/review_cubit.dart';
+import 'package:Basera/features/Course/presentation/views/about_section.dart';
 import 'package:Basera/features/Course/presentation/views/widgets/about_course_text_icon.dart';
 import 'package:Basera/features/Course/presentation/views/widgets/comment_rate_list_view.dart';
 import 'package:Basera/features/Course/presentation/views/widgets/courseRating.dart';
@@ -21,8 +23,9 @@ class AboutSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 16),
+      padding: const EdgeInsets.only(top: 16 , left: 20,right: 20),
       child: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -100,9 +103,7 @@ class AboutSection extends StatelessWidget {
               height: 8,
             ),
             CustomFilledButton(
-              onPressed: () {
-                showReviewDialog(context);
-              },
+              onPressed: ()=> showReviewDialog(context, course),
               title: 'اضافة مراجعة',
               width: SizeConfig.screenWidth,
               color: Theme.of(context).colorScheme.primary,
@@ -121,67 +122,3 @@ class AboutSection extends StatelessWidget {
   }
 }
 
-void showReviewDialog(BuildContext context) {
-  ReviewCubit reviewCubit = context.read<ReviewCubit>();
-
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return BlocProvider<ReviewCubit>.value(
-        value: reviewCubit,
-        child: AlertDialog(
-          backgroundColor: Theme.of(context).colorScheme.surface,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          title: Text("تقييم الكورس",
-              textAlign: TextAlign.center,
-              style: Styles.style20(context).copyWith(
-                color: Theme.of(context).colorScheme.onSurface,
-              )),
-          content: BlocBuilder<ReviewCubit, ReviewState>(
-            builder: (context, state) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  RatingBar.builder(
-                    initialRating: reviewCubit.userRating,
-                    minRating: 1,
-                    direction: Axis.horizontal,
-                    allowHalfRating: false,
-                    itemCount: 5,
-                    itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-                    itemBuilder: (context, _) => const Icon(
-                      Icons.star,
-                      color: Colors.amber,
-                    ),
-                    onRatingUpdate: reviewCubit.onRatingUpdate,
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    reviewCubit.ratingMessage,
-                    style: Styles.style18(context).copyWith(
-                        color: Theme.of(context).colorScheme.onSurface),
-                  ),
-                  const SizedBox(height: 20),
-                  CustomTextField(
-                    controller: reviewCubit.commentController,
-                    maxLines: 5,
-                  ),
-                ],
-              );
-            },
-          ),
-          actions: [
-            CustomFilledButton(
-              title: 'أرسال',
-              onPressed: () {},
-              textColor: Theme.of(context).colorScheme.onPrimary,
-              radius: 36,
-            ),
-          ],
-        ),
-      );
-    },
-  );
-}
