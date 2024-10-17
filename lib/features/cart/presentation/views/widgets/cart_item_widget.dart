02fad09f8de186f2b572e006e21ed3b8c4e6bd51
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-17 23:02:45 +0200

    added navigate to to course from cart

--- lib/features/cart/presentation/views/widgets/cart_item_widget.dart
+++ lib/features/cart/presentation/views/widgets/cart_item_widget.dart
@@ -1,5 +1,10 @@
+import 'dart:developer';
+
 import 'package:Basera/core/models/course.dart';
+import 'package:Basera/core/utility/functions/navigate_functions.dart';
 import 'package:Basera/core/widgets/custom_divider.dart';
+import 'package:Basera/features/Course/data/get_course_by_id.dart';
+import 'package:Basera/features/Course/presentation/views/course_view.dart';
 import 'package:Basera/features/cart/presentation/views/widgets/cart_item_widget_content.dart';
 import 'package:Basera/features/cart/presentation/views/widgets/cart_item_widget_price_with_remove.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -15,63 +20,72 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20, top: 8),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              CartItemWidgetContent(
-                instructionName: course.instructorName,
-                title: course.title,
-              ),
-              const Spacer(),
-              Container(
-                width: 147,
-                height: 100,
-                clipBehavior: Clip.hardEdge,
-                constraints: const BoxConstraints(
-                  maxWidth: 147,
-                  maxHeight: 100,
+    return GestureDetector(
+      onTap: () async {
+        var result  = await GetCourseByIdService.getCourseById(course.id.toString());
+        result.fold((l){}, (r) {
+          pushReplacement(context,
+            CourseView(course: r));
+        },);
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 8),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                CartItemWidgetContent(
+                  instructionName: course.instructorName,
+                  title: course.title,
                 ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.25),
-                      offset: const Offset(0, 3),
-                      blurRadius: 4,
-                      spreadRadius: 0,
+                const Spacer(),
+                Container(
+                  width: 147,
+                  height: 100,
+                  clipBehavior: Clip.hardEdge,
+                  constraints: const BoxConstraints(
+                    maxWidth: 147,
+                    maxHeight: 100,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.25),
+                        offset: const Offset(0, 3),
+                        blurRadius: 4,
+                        spreadRadius: 0,
+                      ),
+                    ],
+                  ),
+                  child: AspectRatio(
+                    aspectRatio: 2 / 3,
+                    child: CachedNetworkImage(
+                      imageUrl: course.image,
+                      placeholder: (context, url) => Shimmer.fromColors(
+                          baseColor: Theme.of(context).colorScheme.secondary,
+                          highlightColor: Theme.of(context).colorScheme.secondary.withOpacity(0.4),
+                          child: Container()),
+                      // TODO LATER ADD ERROR IMAGE (SAYING NO IMAGE)
+                      errorWidget: (context, url, error) => Container(),
                     ),
-                  ],
-                ),
-                child: AspectRatio(
-                  aspectRatio: 2 / 3,
-                  child: CachedNetworkImage(
-                    imageUrl: course.image,
-                    placeholder: (context, url) => Shimmer.fromColors(
-                        baseColor: Theme.of(context).colorScheme.secondary,
-                        highlightColor: Theme.of(context).colorScheme.secondary.withOpacity(0.4),
-                        child: Container()),
-                    // TODO LATER ADD ERROR IMAGE (SAYING NO IMAGE)
-                    errorWidget: (context, url, error) => Container(),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-         CartItemWidgetPriceWithRemove(course: course,),
-        const SizedBox(height: 8),
-        const CustomDivider(),
-      ],
+          const SizedBox(
+            height: 8,
+          ),
+           CartItemWidgetPriceWithRemove(course: course,),
+          const SizedBox(height: 8),
+          const CustomDivider(),
+        ],
+      ),
     );
   }
 }
