1dc8dd8bfdc822e9843830fd048bc865a72a0255
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-19 16:00:41 +0200

    added logic for similar courses

--- lib/features/Course/presentation/views/similar_courses_section.dart
+++ lib/features/Course/presentation/views/similar_courses_section.dart
@@ -1,19 +1,59 @@
+import 'package:Basera/core/models/course.dart';
+import 'package:Basera/core/utility/size_config.dart';
+import 'package:Basera/core/widgets/custom_card_list_view_shimmer.dart';
+import 'package:Basera/core/widgets/custom_empty_state_widget.dart';
+import 'package:Basera/core/widgets/custom_error_widget.dart';
+import 'package:Basera/features/Course/presentation/manager/filter_courses_cubit/filter_courses_cubit.dart';
 import 'package:Basera/features/Course/presentation/views/widgets/similar_courses_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SimilarCoursesSection extends StatelessWidget {
-  const SimilarCoursesSection({super.key});
+class SimilarCoursesSection extends StatefulWidget {
+  final Course course;
+
+  const SimilarCoursesSection({super.key, required this.course});
 
   @override
+  State<SimilarCoursesSection> createState() => _SimilarCoursesSectionState();
+}
+
+class _SimilarCoursesSectionState extends State<SimilarCoursesSection> {
+  @override
+  void initState() {
+    context.read<FilterCoursesCubit>().getSimilarCourses(widget.course);
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
-    return  ListView.builder(
-      padding: const EdgeInsets.only(top: 16),
-      itemCount: 10,
-      itemBuilder: (context, index) => const Padding(
-        padding: EdgeInsets.only(bottom: 10),
-        child: SimilarCoursesItem(),
-      ),
+    var filterCoursesCubit = context.read<FilterCoursesCubit>();
+    return BlocBuilder<FilterCoursesCubit, FilterCoursesState>(
+      builder: (context, state) {
+        if (state is FilterCoursesFailure) {
+          return CustomErrorWidget(text: state.message);
+        } else if (state is FilterCoursesSuccess) {
+          if (filterCoursesCubit.similarCourses.isEmpty) {
+            return CustomEmptyStateWidget(
+                title: 'لا يوجد كورسات مشابهة',
+                subTitle: 'سنقوم بأضافة المزيد ..',
+                image: Image.asset(
+                  'lib/assets/images/lessons-empty-state.png',
+                  width: SizeConfig.screenWidth * 0.8,
+                  height: SizeConfig.screenHeight * 0.3,
+                ));
+          }
+          filterCoursesCubit.similarCourses.removeWhere((element) => element.id == widget.course.id,);
+          return ListView.builder(
+            padding: const EdgeInsets.only(top: 16, left: 20, right: 20),
+            itemCount: filterCoursesCubit.similarCourses.length,
+            itemBuilder: (context, index) =>  Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: SimilarCoursesItem(course: filterCoursesCubit.similarCourses[index],),
+            ),
+          );
+        } else {
+          return const CustomCardListViewShimmer();
+        }
+      },
     );
   }
 }
-
--- lib/features/Course/presentation/views/widgets/similar_courses_item.dart
+++ lib/features/Course/presentation/views/widgets/similar_courses_item.dart
@@ -1,35 +1,55 @@
+import 'package:Basera/core/models/course.dart';
+import 'package:Basera/core/utility/functions/navigate_functions.dart';
 import 'package:Basera/core/utility/styles.dart';
 import 'package:Basera/core/widgets/rate_widget.dart';
+import 'package:Basera/features/Course/presentation/views/course_view.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class SimilarCoursesItem extends StatelessWidget {
+  final Course course;
   const SimilarCoursesItem({
-    super.key,
+    super.key, required this.course,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start  ,
-          children: [
-            Text('مقدمة في علوم الحاسوب',style: Styles.style16(context).copyWith(fontWeight: FontWeight.w600),),
-            const SizedBox(height: 4,),
-            const RateWidget(),
-            Text('10000 طالب',style: Styles.style16(context),),
-          ],
-        ),
-        Container(
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-            ),
-            width: 85,
-            height: 85,
-            child: Image.asset('lib/assets/images/course-image.png',fit: BoxFit.cover,)),
-      ],
+    return GestureDetector(
+      onTap: () => pushReplacement(context, CourseView(course: course)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start  ,
+            children: [
+              Text(course.title,style: Styles.style16(context).copyWith(fontWeight: FontWeight.w600),),
+              const SizedBox(height: 4,),
+               RateWidget(rate: course.averageRating ?? 0,),
+              Text('${course.enrollmentCount} طالب',style: Styles.style16(context),),
+            ],
+          ),
+          Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              width: 85,
+              height: 85,
+              child: CachedNetworkImage(imageUrl: course.image,fit: BoxFit.cover,
+              placeholder: (context, url) => Shimmer.fromColors(
+                baseColor: Colors.grey.shade300,
+                highlightColor: Colors.grey.shade100,
+                child: Container(
+                  width: 85,
+                  height: 85,
+                  color: Colors.white,
+                ),
+              ),
+              ),
+          ),
+        ],
+      ),
     );
   }
 }
