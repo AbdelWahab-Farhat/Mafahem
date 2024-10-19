6ec482b702ab1542a28b900bddecbfa9bcbc6558
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-19 15:57:58 +0200

    new view with the logic in it

--- lib/features/category/views/widgets/category_item.dart
+++ lib/features/category/views/widgets/category_item.dart
@@ -1,5 +1,7 @@
 import 'package:Basera/core/models/category.dart';
+import 'package:Basera/core/utility/functions/navigate_functions.dart';
 import 'package:Basera/core/utility/size_config.dart';
+import 'package:Basera/features/category/views/category_courses_view.dart';
 import 'package:Basera/features/category/views/widgets/category_stacked_row.dart';
 import 'package:flutter/material.dart';
 
@@ -12,23 +14,30 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-      ),
-      width: SizeConfig.screenWidth * 0.37,
-      height: 100,
-      child: Stack(
-        children: [
-          Image.asset(
-            'lib/assets/images/Roadmap-to-Learn-JavaScript-For-Beginners 1.png',
-            fit: BoxFit.cover,
-            width: SizeConfig.screenWidth,
-            height: SizeConfig.screenHeight,
-          ),
-           CategoryStackedRow(category: category,)
-        ],
+    return GestureDetector(
+      onTap: () async {
+        push(context, CategoryCoursesView(category: category,));
+      },
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        width: SizeConfig.screenWidth * 0.37,
+        height: 100,
+        child: Stack(
+          children: [
+            Image.asset(
+              'lib/assets/images/Roadmap-to-Learn-JavaScript-For-Beginners 1.png',
+              fit: BoxFit.cover,
+              width: SizeConfig.screenWidth,
+              height: SizeConfig.screenHeight,
+            ),
+            CategoryStackedRow(
+              category: category,
+            )
+          ],
+        ),
       ),
     );
   }
