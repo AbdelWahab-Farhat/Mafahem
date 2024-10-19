5bbba13ca72b10b359045f3bddc380267d40b178
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-19 16:04:23 +0200

    added with and height for empty state photo

--- lib/features/Course/presentation/views/lecture_section.dart
+++ lib/features/Course/presentation/views/lecture_section.dart
@@ -1,14 +1,19 @@
 import 'dart:developer';
 
+import 'package:Basera/core/enums/course_status.dart';
 import 'package:Basera/core/models/course.dart';
 import 'package:Basera/core/models/lesson.dart';
 import 'package:Basera/core/utility/functions/navigate_functions.dart';
+import 'package:Basera/core/utility/functions/ui_functions.dart';
 import 'package:Basera/core/utility/size_config.dart';
 import 'package:Basera/core/utility/styles.dart';
 import 'package:Basera/core/widgets/custom_divider.dart';
 import 'package:Basera/core/widgets/custom_empty_state_widget.dart';
+import 'package:Basera/features/Course/presentation/manager/course_cubit/course_cubit.dart';
 import 'package:Basera/features/Course/presentation/views/lesson_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LectureSection extends StatelessWidget {
   final Course course;
@@ -20,7 +25,11 @@ class LectureSection extends StatelessWidget {
       return CustomEmptyStateWidget(
           title: 'لاتوجد دروس',
           subTitle: 'سيتم أضافة الدروس قريبا',
-          image: Image.asset('lib/assets/images/lessons-empty-state.png',width: SizeConfig.screenWidth * 0.8,height: SizeConfig.screenHeight * 0.3,));
+          image: Image.asset(
+            'lib/assets/images/lessons-empty-state.png',
+            width: SizeConfig.screenWidth * 0.8,
+            height: SizeConfig.screenHeight * 0.3,
+          ));
     }
     return LessonsListView(course: course);
   }
@@ -64,11 +73,14 @@ class LessonItem extends StatelessWidget {
   const LessonItem({
     super.key,
     required this.lesson,
-    required this.index, required this.course,
+    required this.index,
+    required this.course,
   });
 
   @override
   Widget build(BuildContext context) {
+      var courseCubit = context.read<CourseCubit>();
+
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       decoration: BoxDecoration(
@@ -96,23 +108,42 @@ class LessonItem extends StatelessWidget {
           style: Styles.style18(context)
               .copyWith(color: Theme.of(context).colorScheme.onPrimary),
         ),
-        children: allLectures(context, lesson, course),
+        children: allLectures(context, lesson, course, courseCubit),
       ),
     );
   }
 }
 
-List<Widget> allLectures(BuildContext context, Lesson lesson, Course course) {
+List<Widget> allLectures(BuildContext context, Lesson lesson, Course course,
+    CourseCubit courseCubit) {
   List<Widget> lectures = [];
   log(lesson.lectures.toString());
   for (var lecture in lesson.lectures!) {
     lectures.add(GestureDetector(
-      onTap: () {
-        push(context, LessonView(lesson: lesson, course: course, lecture: lecture,));
-      },
+      onTap: courseCubit.courseStatus != CourseStatus.purchased
+          ? () {
+              showCustomSnackBar(
+                  context, 'لا يمكنك تشغيل الدرس لانك لم تقم بشراء الكورس');
+            }
+          : () {
+              push(
+                  context,
+                  LessonView(
+                    lesson: lesson,
+                    course: course,
+                    lecture: lecture,
+                  ));
+            },
       child: Column(
         children: [
           ListTile(
+            trailing: courseCubit.courseStatus != CourseStatus.purchased
+                ?  Icon(
+                    FontAwesomeIcons.lock,
+                    size: 15,
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  )
+                : null,
             title: Text(
               maxLines: 1,
               lecture.title,
