59bffeac421b5ec215eafe4a4f5a9262129ca047
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-10-19 16:00:10 +0200

    useless class but  maybe i need it for future.

---
+++ lib/features/Course/data/get_full_review.dart
@@ -0,0 +1,21 @@
+import 'package:Basera/core/models/rater.dart';
+import 'package:Basera/core/models/review.dart';
+
+
+// NO NEED IT FOR NOW
+class GetFullReview {
+  static Map<Rater, Review> getUserFullReview(
+      List<Review> reviews, List<Rater> raters) {
+    {
+      Map<Rater, Review> map = {};
+      for (var review in reviews) {
+        for (var rater in raters) {
+          if (rater.pivot.userId == review.userId) {
+            map[rater] = review;
+          }
+        }
+      }
+      return map;
+    }
+  }
+}
